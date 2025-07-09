e3ec175c49e3c01686a535ca1efbbd4b99bd49ea
leangvakhim <Vakhiml60@gmail.com>
2025-07-09 21:15:42 +0700

    Update swagger-initializer.js

--- swagger/swagger-initializer.js
+++ swagger/swagger-initializer.js
@@ -3,7 +3,7 @@ window.onload = function() {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "/phppost/api/openapi.json",
+    url: "https://api.aimostore.shop/php/phppost/api/openapi.json",
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
